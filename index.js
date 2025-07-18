61381de1891d881d18225d39e9e9db5e0aaea16a
gumaygo <ilham_gumay@outlook.com>
2025-07-18 20:38:29 +0700

    change info

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import fs from 'fs';
 let localChain = [];
 try {
   localChain = JSON.parse(fs.readFileSync('./chain/chain.json', 'utf-8'));
+  console.log(`Chain lokal sebelum sync: ${localChain.length} block`);
 } catch {
   localChain = [];
+  console.log('Chain lokal tidak ditemukan, mulai dari kosong.');
 }
 
-syncChain(localChain);
+syncChain(localChain).then(() => {
+  const updatedChain = JSON.parse(fs.readFileSync('./chain/chain.json', 'utf-8'));
+  console.log(`Chain lokal setelah sync: ${updatedChain.length} block`);
+  process.exit(0);
+});
